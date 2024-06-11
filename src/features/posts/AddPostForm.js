8d81eba5ecb21cad4f0e07c4ae5b7bcd5615f999
jasongoose <ssjk7789@gmail.com>
2024-06-11 22:01:54 +0900

    AddPostForm 컴포넌트 정의 및 적용

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+export default function AddPostForm() {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button type="button">Save Post</button>
+      </form>
+    </section>
+  )
+}
